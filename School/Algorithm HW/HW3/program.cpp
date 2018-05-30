3d0814289693c4829eafacafd0d8e1ae50c1d630
fw93 <okjinhyuk@snu.ac.kr>
2018-05-30 16:33:00 +0900

    matrix scc version testing complete

--- School/Algorithm HW/HW3/program.cpp
+++ School/Algorithm HW/HW3/program.cpp
@@ -19,9 +19,107 @@ void sccmatrix(vector<vector<int>> & matrix){
     // check time
     chrono::time_point<chrono::high_resolution_clock> start, end;
     start = chrono::high_resolution_clock::now();
+        
+    // the node stack we want to obtain
+    stack<int> vert;
+    // stack resulting by dfs
+    stack<int> tmp;
+    // recurring stack
+    stack<int> buffer;
+    // visited vector
+    vector<int> visited(matrix.size(),0);
+
+    // do dfs and gather terminating sequence
+    stack<int> path;
+    for(int i = 0; i < matrix.size() ; i++){
+        if(visited[i]==0){
+            int curr=i;
+            path.push(i);
+            visited[i]=1;
+            while(path.size()!=0){
+                int flag = 0;
+                for(int j = 0 ; j < matrix.size();j++){
+                    if(matrix[curr][j]==1&&visited[j]==0){
+                        path.push(j);
+                        flag = 1;
+                        visited[path.top()]=1;
+                        curr = path.top();
+                        break;
+                    }
+                }
+                if(flag==0){
+                    // nowhere to go!
+                    vert.push(curr);
+                    path.pop();
+                    if(path.size()!=0) curr = path.top();
+                }
+            }
+        }
+    }
+
+    // make a reverted graph
+    vector<vector<int>> trans(matrix.size(),vector<int>(matrix.size(),0));
+
+    for(int i = 0 ; i < matrix.size() ; i++){
+        for(int j = 0 ; j < matrix.size() ; j++){
+            trans[j][i]=matrix[i][j];
+        }
+    }
+
+    // do dfs on trans by vert order
+    for(int i = 0 ; i < visited.size() ; i++){
+        visited[i]=0;
+    }
+
+    vector<vector<int>> result;
+
+    while(vert.size()!=0){
+        int i = vert.top();
+        vert.pop();
+        if(visited[i]==0){
+            buffer.push(i);
+            result.push_back(vector<int>());
+            while(buffer.size()!=0){
+                int curr = buffer.top();
+                if(visited[curr]==0){
+                    result[result.size()-1].push_back(buffer.top());
+                    visited[curr]=1;
+                }
+                buffer.pop();
+                for(int j = 0 ; j < trans[curr].size() ; j++){
+                    if(trans[curr][j]==1&&visited[j]==0){
+                        buffer.push(j);
+                    }
+                }
+            }
+        }
+    }
     
-    // print time
     end = chrono::high_resolution_clock::now();
+
+
+    // sort and print result
+    for(int i = 0 ; i < result.size() ; i++){
+        sort(result[i].begin(),result[i].end());
+    }
+
+    sort(result.begin(),result.end(),custom);
+
+    ofstream out("out.txt");
+
+    for(int i = 0 ; i < result.size() ; i++){
+        for(int j = 0 ; j < result[i].size();j++){
+            if(j==result[i].size()-1){
+                out << (result[i][j]+1) << " " << endl;
+            }else{
+                out << (result[i][j]+1) << " " ;
+            }
+        }
+    }
+
+    out.close();
+
+    // print time
     chrono::duration<double> elapsed_seconds = end - start;
     cout << elapsed_seconds.count() << "s" << endl;
 
@@ -199,8 +297,8 @@ int main(int argc, char ** argv){
     // print results
 
     sccmatrix(matrix);
-    scclist(adjlist);
-    sccarray(arref, arseq);
+    //scclist(adjlist);
+    //sccarray(arref, arseq);
 
     return 0;
 }
