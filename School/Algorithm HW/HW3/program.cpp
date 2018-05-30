4a1b5f613c3666d3a099fe2feef2fb46c072f73c
fw93 <okjinhyuk@snu.ac.kr>
2018-05-30 11:59:40 +0900

    woking on prototype debugging. battery out

--- School/Algorithm HW/HW3/program.cpp
+++ School/Algorithm HW/HW3/program.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 bool custom(vector<int> a, vector<int> b){
-    return a[0]>b[0];
+    return a[0]<b[0];
 }
 
 void sccmatrix(vector<vector<int>> & matrix){
@@ -23,6 +23,15 @@ void sccmatrix(vector<vector<int>> & matrix){
     chrono::duration<double> elapsed_seconds = end - start;
     cout << elapsed_seconds.count() << "s" << endl;
 
+}
+
+void filldfs(int curr, vector<int> & visited, vector<vector<int>> & list, stack<int> & vert){
+    if(visited[curr]==1) return;
+    visited[curr]=1;
+    for(int i = 0 ;i<list[curr].size();i++){
+        if(visited[list[curr][i]]==0) filldfs(list[curr][i],visited,list,vert);
+    }
+    vert.push(curr);
 }
 
 void scclist(vector<vector<int>> & list){
@@ -41,13 +50,15 @@ void scclist(vector<vector<int>> & list){
     vector<int> visited(list.size(),0);
 
     // do dfs and gather terminating sequence
-    for(int i = 0; i < list.size() ; i++){
+/*    for(int i = 0; i < list.size() ; i++){
         if(visited[i]==0){
             buffer.push(i);
             while(buffer.size()!=0){
-                tmp.push(buffer.top());
                 int curr = buffer.top();
-                visited[curr]=1;
+                if(visited[curr]==0){
+                    visited[curr]=1;
+                    tmp.push(buffer.top());
+                }
                 buffer.pop();
                 for(int j = 0 ; j < list[i].size() ; j++){
                     if(visited[list[i][j]]==0){
@@ -59,6 +70,28 @@ void scclist(vector<vector<int>> & list){
     }
 
     // fill vert buffer
+    cout << "see tmp buffer" << endl;
+
+    while(tmp.size()!=0){
+        vert.push(tmp.top());
+        cout << tmp.top() << " ";
+        tmp.pop();
+    }
+
+    cout << endl;
+*/
+
+    for(int i = 0 ; i < list.size() ; i++){
+        if(visited[i]==0) filldfs(i,visited,list,vert);
+    }
+    
+    cout << "see vert" << endl;
+    while(vert.size()!=0){
+        tmp.push(vert.top());
+        cout << vert.top() << " ";
+        vert.pop();
+    }
+    cout << endl;
     while(tmp.size()!=0){
         vert.push(tmp.top());
         tmp.pop();
@@ -73,29 +106,46 @@ void scclist(vector<vector<int>> & list){
         }
     }
 
+    cout << "see trans buffer" << endl;
+    for(int i = 0 ; i < trans.size() ; i++){
+        for(int j = 0 ; j < trans[i].size() ; j++){
+            cout << trans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     // do dfs on trans by vert order
 
-    for(int x : visited) x = 0;
+    for(int i = 0 ; i < visited.size() ; i++){
+        visited[i]=0;
+    }
 
     vector<vector<int>> result;
 
-    for(int i = 0 ; i < list.size() ; i++){
+    while(vert.size()!=0){
+        int i = vert.top();
+        vert.pop();
         if(visited[i]==0){
             buffer.push(i);
             result.push_back(vector<int>());
             while(buffer.size()!=0){
-                result[result.size()-1].push_back(buffer.top());
                 int curr = buffer.top();
+                if(visited[curr]==0){
+                    result[result.size()-1].push_back(buffer.top());
+                    visited[curr]=1;
+                }
                 buffer.pop();
-                visited[curr]=1;
-                for(int j = 0 ; j < result[i].size() ; j++){
-                    if(visited[result[i][j]]==0){
-                        buffer.push(result[i][j]);
+                for(int j = 0 ; j < trans[i].size() ; j++){
+                    if(visited[trans[i][j]]==0){
+                        buffer.push(trans[i][j]);
                     }
                 }
             }
         }
     }
+    // print time
+    end = chrono::high_resolution_clock::now();
+
 
     // sort and print result
     for(int i = 0 ; i < result.size() ; i++){
@@ -104,23 +154,25 @@ void scclist(vector<vector<int>> & list){
 
     sort(result.begin(),result.end(),custom);
 
-    cout << "i was here" << endl;
+    cout << "print result" << endl;
+    ofstream out("out.txt");
 
     for(int i = 0 ; i < result.size() ; i++){
         for(int j = 0 ; j < result[i].size();j++){
             if(j==result[i].size()-1){
-                cout << result[i][j]<<endl;
+                cout << (result[i][j]+1) << endl;
+                out << (result[i][j]+1) << endl;
             }else{
-                cout << result[i][j] << " " ;
+                cout << (result[i][j]+1) << " " ;
+                out << (result[i][j]+1) << " " ;
             }
         }
     }
 
-    // print time
-    end = chrono::high_resolution_clock::now();
+    out.close();
+
     chrono::duration<double> elapsed_seconds = end - start;
     cout << elapsed_seconds.count() << "s" << endl;
-
 }
 
 void sccarray(vector<int> & ref, vector<int> & seq){
@@ -154,7 +206,7 @@ int main(int argc, char ** argv){
     openfile >> n;
     vector<vector<int>> matrix(n,vector<int>(n,0));
     vector<vector<int>> adjlist(n,vector<int>()); 
-    vector<int> arref();
+    vector<int> arref;
     vector<int> arseq;
 
     for(int i = 0 ; i < n ; i++){
@@ -163,6 +215,7 @@ int main(int argc, char ** argv){
         for(int j = 0 ; j < x ; j++){
             int tmp;
             openfile >> tmp;
+            tmp--;
             matrix[i][tmp]=1;
             adjlist[i].push_back(tmp);
             arseq.push_back(tmp);
@@ -171,13 +224,15 @@ int main(int argc, char ** argv){
         else arref.push_back(0);
     }
     
+    cout << "see matrix" << endl;
     for(int i = 0 ; i < matrix.size() ; i++){
         for(int j = 0 ; j < matrix[i].size(); j++){
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
-
+    
+    cout << "see list" << endl;
     for(int i = 0 ; i < adjlist.size() ; i++){
         for(int j = 0 ; j < adjlist[i].size() ; j++){
             cout << adjlist[i][j] << " ";
@@ -185,6 +240,7 @@ int main(int argc, char ** argv){
         cout << endl;
     }
 
+    cout << "see arr" << endl;
     for(int x : arseq) cout << x << " ";
     cout << endl;
     for(int x : arref) cout << x << " ";
