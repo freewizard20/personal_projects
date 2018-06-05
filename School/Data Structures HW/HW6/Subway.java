542dca39678890042a27fb8df5974ffb71fb1f8c
Jinhyuk Jeon <okjinhyuk@snu.ac.kr>
2018-06-05 19:18:50 +0900

    starting hw6

---
+++ School/Data Structures HW/HW6/Subway.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+
+public class Subway{
+    
+    public static void main(String[] args){
+        // parse data
+        
+    }
+}
+
+class db{
+    
+    // data structure containing subway information
+    private Vector<Integer> ref;
+    private Vector<Integer> seq;
+    
+    public graph(Vector<Integer> r, Vector<Integer> s){
+        ref = r;
+        seq = s;
+    }
+    
+    // shortest path
+    public void goto(String from, String to){
+
+    }
+
+    // shortest transfers
+    public void goto2(String from, String to){
+
+    }
+    
+}
