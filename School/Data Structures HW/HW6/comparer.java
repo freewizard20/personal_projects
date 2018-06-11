0f38d34b57fa0d78e02497e3f99589eb1eb47b81
Jinhyuk Jeon <okjinhyuk@snu.ac.kr>
2018-06-11 22:06:17 +0900

    done with all tests

--- School/Data Structures HW/HW6/comparer.java
+++ School/Data Structures HW/HW6/comparer.java
@@ -12,12 +12,21 @@ public class comparer{
     public static void main(String[] args)throws FileNotFoundException, IOException,UnsupportedEncodingException{
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]),"UTF8"));
         BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream(args[1]),"UTF8"));
-        for(int i = 0 ; i < 10000 ; i++){
-            br.readLine();
+        int i = 0;
+        while(true){
+            try{
+            String x = br.readLine();
+            if(x==null) return;
             br2.readLine();
             int curr1 = Integer.parseInt(br.readLine());
             int curr2 = Integer.parseInt(br2.readLine());
             if(curr1!=curr2) System.out.println("Error on " + i);
+            i++;
+            }catch(Exception e){
+                System.out.println(i);
+                System.out.println(e);
+                return;
+            }
         }
     }
 }
